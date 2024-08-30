f5d0450d8ed426d2c3fb671c11be20555bc916ee
subbni <slfkak@naver.com>
2024-08-30 15:51:09 +0900

    [add] 로그아웃 구현

--- blog-frontend/src/modules/user.js
+++ blog-frontend/src/modules/user.js
@@ -1,20 +1,21 @@
 import { createAction, handleActions } from 'redux-actions';
-import { takeLatest } from 'redux-saga/effects';
+import { call, takeLatest } from 'redux-saga/effects';
 import createRequestSaga, {
 	createRequestActionTypes,
 } from '../lib/createRequestSaga';
 import * as authAPI from '../lib/api/auth';
-const TEMP_SET_USER = 'user/TEMP_SET_USER'; // 새로고침 이후 임시 로그인 처리
 
+const TEMP_SET_USER = 'user/TEMP_SET_USER'; // 새로고침 이후 임시 로그인 처리
 // 회원 정보 확인
 const [CHECK, CHECK_SUCCESS, CHECK_FAILURE] =
 	createRequestActionTypes('user/CHECK');
+const LOGOUT = 'user/LOGOUT';
 
 export const tempSetUser = createAction(TEMP_SET_USER, (user) => user);
 export const check = createAction(CHECK);
+export const logout = createAction(LOGOUT);
 
 const checkSaga = createRequestSaga(CHECK, authAPI.check);
-
 function checkFailureSaga() {
 	try {
 		localStorage.removeItem('user');
@@ -23,9 +24,19 @@ function checkFailureSaga() {
 	}
 }
 
+function* logoutSaga() {
+	try {
+		yield call(authAPI.logout);
+		localStorage.removeItem('user');
+	} catch (e) {
+		console.log(e);
+	}
+}
+
 export function* userSaga() {
 	yield takeLatest(CHECK, checkSaga);
 	yield takeLatest(CHECK_FAILURE, checkFailureSaga);
+	yield takeLatest(LOGOUT, logoutSaga);
 }
 
 const initialState = {
@@ -48,6 +59,10 @@ export default handleActions(
 			...state,
 			user: null,
 			checkError: error,
+		}),
+		[LOGOUT]: (state) => ({
+			...state,
+			user: null,
 		}),
 	},
 	initialState,
