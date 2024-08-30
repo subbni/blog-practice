183a44f0eae9b03a802418d2d2cafd44d59c6d9c
subbni <slfkak@naver.com>
2024-08-30 13:34:38 +0900

    [fix] 오타 수정

--- blog-frontend/src/modules/user.js
+++ blog-frontend/src/modules/user.js
@@ -1,5 +1,5 @@
 import { createAction, handleActions } from 'redux-actions';
-import { takeLatest } from 'redux-saga/effecs';
+import { takeLatest } from 'redux-saga/effects';
 import createRequestSaga, {
 	createRequestActionTypes,
 } from '../lib/createRequestSaga';
@@ -23,20 +23,22 @@ const initialState = {
 	checkError: null,
 };
 
-export default handleActions({
-	[TEMP_SET_USER]: (state, { payload: user }) => ({
-		...state,
-		user,
-	}),
-	[CHECK_SUCCESS]: (state, { payload: user }) => ({
-		...state,
-		user,
-		checkError: null,
-	}),
-	[CHECK_FAILURE]: (state, { payload: error }) => ({
-		...state,
-		user: null,
-		checkError: error,
-	}),
+export default handleActions(
+	{
+		[TEMP_SET_USER]: (state, { payload: user }) => ({
+			...state,
+			user,
+		}),
+		[CHECK_SUCCESS]: (state, { payload: user }) => ({
+			...state,
+			user,
+			checkError: null,
+		}),
+		[CHECK_FAILURE]: (state, { payload: error }) => ({
+			...state,
+			user: null,
+			checkError: error,
+		}),
+	},
 	initialState,
-});
+);
